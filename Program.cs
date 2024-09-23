6be9298aef40c05b472f14971ac2071ede75b550
AldiiX <aldiix@email.cz>
2024-09-23 17:26:59 +0200

    3.0
    - trochu updatnutý template

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using dotenv.net;
+using WebApplication.Middlewares;
+using WebApplication.Services;
 
 namespace WebApplication;
 
@@ -10,16 +12,66 @@ public static class Program {
     public static IDictionary<string, string> Env { get; private set; } = null!;
     public static ILogger Logger => App.Logger;
 
+    
+    #if DEBUG || TESTING
+        public const bool DEVELOPMENT_MODE = true;
+    #else
+        public const bool DEVELOPMENT_MODE = false;
+    #endif
 
+    
+    
 
     public static void Main(string[] args) {
         var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
 
         builder.Services.AddControllersWithViews();
+        builder.Services.AddHttpContextAccessor();
+        /*builder.Services.AddStackExchangeRedisCache(options => {
+            if (DEVELOPMENT_MODE) {
+                options.ConfigurationOptions = new ConfigurationOptions {
+                    EndPoints = { $"{ENV["DATABASE_IP"]}:{ENV["REDIS_PORT"]}" },
+                    Password = ENV["REDIS_PUBLICACC_PASSWORD"],
+                };
+            } else options.Configuration = "localhost:6379";
+
+            options.InstanceName = "MySalonMostSession";
+        });
+        builder.Services.AddSession(options => {
+            options.IdleTimeout = TimeSpan.FromDays(365);
+            options.Cookie.HttpOnly = true;
+            options.Cookie.IsEssential = true;
+
+            options.Cookie.MaxAge = TimeSpan.FromDays(365); // Trvání cookie na 365 dní
+            //options.Cookie.Expiration = TimeSpan.FromDays(365);
+            options.Cookie.Name = "mysalonmost_session";
+        });*/
+
+        builder.Configuration
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        #if DEBUG
+                builder.Configuration.AddJsonFile("appsettings.Debug.json", optional: true, reloadOnChange: true);
+        #elif RELEASE
+                builder.Configuration.AddJsonFile("appsettings.Release.json", optional: true, reloadOnChange: true);
+        #elif TESTING
+            builder.Configuration.AddJsonFile("appsettings.Testing.json", optional: true, reloadOnChange: true);
+        #endif
+
+        builder.Configuration.AddEnvironmentVariables();
 
         App = builder.Build();
         Env = DotEnv.Read();
+        
+        
+        
+        // Konfigurace HttpContextService
+        var httpContextAccessor = App.Services.GetRequiredService<IHttpContextAccessor>();
+        HttpContextService.Configure(httpContextAccessor);
+        
 
+        
         // Configure the HTTP request pipeline.
         if (!App.Environment.IsDevelopment()) {
             App.UseExceptionHandler("/error");
@@ -27,20 +79,17 @@ public static class Program {
             App.UseHsts();
         }
 
+        
+        
         App.UseHttpsRedirection();
         App.UseStaticFiles();
-
+        //App.UseSession();
         App.UseRouting();
         App.UseAuthorization();
-
+        App.UseMiddleware<ErrorHandlingMiddleware>();
         App.MapControllerRoute(name: "default", pattern: "/");
 
-        App.Use(async (context, next) => {
-            await next();
-
-            // Pokud není nalezena žádná odpovídající cesta
-            if (context.Response is { StatusCode: 404, HasStarted: false }) context.Response.Redirect("/");
-        });
+        
 
         // Spuštění aplikace
         App.Run();
